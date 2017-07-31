88b17ee704bbd21a10bb0b10fc6db98afd51d91b
DmitryBilevich <dmitry.bilevich@gmail.com>
2017-07-31 18:55:16 +0300

    Add new method GetBook

--- TestRest2/Controllers/SectionController.cs
+++ TestRest2/Controllers/SectionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using TestRest2.Models;
 
 namespace TestRest2.Controllers
 {
@@ -17,6 +18,12 @@ namespace TestRest2.Controllers
         public int GetNum()
         {
             return 4;
+        }
+
+        public Book GetBook()
+        {
+            Book book =new Book();
+            return book;
         }
     }
 }
