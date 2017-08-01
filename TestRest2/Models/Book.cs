c4447eb2c5a91e31d041cad4f5a2f594a4724767
DmitryBilevich <dmitry.bilevich@gmail.com>
2017-08-01 18:36:43 +0300

    Add NewController
    add NewController

--- TestRest2/Models/Book.cs
+++ TestRest2/Models/Book.cs
@@ -7,5 +7,9 @@ namespace TestRest2.Models
 {
     public class Book
     {
+        public string Name { get; set; }
+        public  string Author { get; set; }
+        public int Id { get; set; }
+        public  string Genre { get; set; }
     }
 }
